0fc83c0b17285078631d0227de3a72ac7db233fe
Ryan Osten <ostenrj@gmail.com>
2017-09-16 18:50:43 -0400

    added query for overdue books

--- routes/books.js
+++ routes/books.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var router = express.Router();
 var Book = require("../models").Book;
+var Loan = require("../models").Loan;
+var moment = require('moment');
+
+const today = moment().format('YYYY[-]MM[-]DD');
 
 /* GET all books */
 router.get('/', function(req, res, next) {
@@ -17,5 +21,25 @@ router.get('/', function(req, res, next) {
     res.render('all_books', {books_arr: books_arr});
   });
 });
+
+/* GET overdue Books*/
+
+router.get('/overdue', function(req, res, next) {
+  Loan.findAll({
+    where: {
+      returned_on: null,
+      return_by: {
+        lt: today
+      }
+    }
+  })
+    .then((books_result) => {
+  let overdue_books = JSON.stringify(books_result);
+  console.log(JSON.parse(overdue_books));
+  res.send('a-ok');
+  });
+});
+
+//where return_by < DATE.now && returned_on == null
 
 module.exports = router;
